e9083677358bfc98c559ec07b3162a91162c5d4a
Simon Paul <samonpaul06@gmail.com>
2024-01-10 17:28:33 +0530

    minor

--- config/connectToDb.js
+++ config/connectToDb.js
@@ -4,10 +4,7 @@ dotenv.config();
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        await mongoose.connect(process.env.MONGO_URI)
     }catch(e) {
         console.log(e);
     }
