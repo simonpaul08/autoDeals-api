de3a69f233a19fc44cf36e0f6ad0462b341eaa2a
Simon Paul <samonpaul06@gmail.com>
2024-01-10 17:44:02 +0530

    update origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:5173",
     "https://auto-deals-client.vercel.app",
-    "https://client-rust-zeta-24.vercel.app"
+    "https://client-rust-zeta-24.vercel.app",
+    "https://mobiledeal-client.vercel.app/"
 ]
 
 export default allowedOrigins;
